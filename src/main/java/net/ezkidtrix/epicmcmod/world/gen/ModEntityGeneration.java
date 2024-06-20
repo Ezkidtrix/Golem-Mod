831e079853804b2e4bfe3ee8c888078136032eb3
BuildTools <unconfigured@null.spigotmc.org>
2024-06-20 08:41:16 -0700

    Added Spawning to the Mini Golem

---
+++ src/main/java/net/ezkidtrix/epicmcmod/world/gen/ModEntityGeneration.java
@@ -0,0 +1,17 @@
+package net.ezkidtrix.epicmcmod.world.gen;
+
+import net.ezkidtrix.epicmcmod.entity.ModEntities;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.minecraft.entity.SpawnGroup;
+import net.minecraft.entity.SpawnRestriction;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.world.Heightmap;
+import net.minecraft.world.biome.BiomeKeys;
+
+public class ModEntityGeneration {
+    public static void addSpawns() {
+        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.FOREST), SpawnGroup.CREATURE, ModEntities.MiniGolem, 30, 1, 1);
+        SpawnRestriction.register(ModEntities.MiniGolem, SpawnRestriction.Location.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, MobEntity::canMobSpawn);
+    }
+}
--- src/main/java/net/ezkidtrix/epicmcmod/world/gen/ModWorldGeneration.java
+++ src/main/java/net/ezkidtrix/epicmcmod/world/gen/ModWorldGeneration.java
@@ -3,5 +3,6 @@ package net.ezkidtrix.epicmcmod.world.gen;
 public class ModWorldGeneration {
     public static void generateModWorldGen() {
         ModOreGeneration.generateOres();
+        ModEntityGeneration.addSpawns();
     }
 }
